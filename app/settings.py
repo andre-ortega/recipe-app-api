b9e70d18bb6421110a42ec2ae4f213dc13f9236d
sansantas <ortegaan@oregonstate.edu>
2022-03-14 22:19:28 -0700

    added flake8 to test script

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+# Copied from other demo project, does nothing! :D
 BASE_DIR=Path(__file__).resolve(strict=True).parent.parent
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
